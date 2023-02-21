2e303b26f03760dbfc2d54445744fb4a395a6eed
Olutomisin Oluwajuwon <jolutomisin@gmail.com>
2023-02-21 23:14:56 +0100

    update mobile view code

--- src/components/Hero/Hero.styles.js
+++ src/components/Hero/Hero.styles.js
@@ -56,4 +56,91 @@ export const Next = styled.div`
 `
 
 export const HeroContent = styled.div`
+    padding: 1rem;
+
+    h1 {
+        padding: 0.5rem 0;
+    }
+    p {
+        color: ${props => props.theme.colors.neutral.darkGrayishBlue};
+        font-size: 15px;
+        line-height: 25px;
+    }
+`
+export const ContentHeader = styled.div`
+    font-size: 14px;
+    font-weight: 700;
+    color: ${props => props.theme.colors.primary.orange};
+`
+export const Counter = styled.div`
+`
+export const Pricing = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 0;
+
+    p {
+        font-weight: 700;
+        text-decoration: line-through;
+        color: ${props => props.theme.colors.neutral.grayishBlue};
+    }
+`
+export const Price = styled.div`
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+`
+export const Span1 = styled.span`
+    font-weight: 700;
+    font-size: 25px;
+`
+export const Span2 = styled.span`
+    font-weight: 700;
+    color: ${props => props.theme.colors.primary.orange};
+    background-color: ${props => props.theme.colors.primary.paleOrange};
+    padding: 0.4rem;
+    border-radius: 5px;
+`
+export const Count = styled.div`
+    background-color: ${props => props.theme.colors.neutral.lightGrayishBlue};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.7rem 1rem;
+    border-radius: 5px;
+
+    span {
+        font-weight: 700;
+    }
+`
+export const Minus = styled.img`
+    cursor: pointer;
+`
+export const Plus = styled.img`
+    cursor: pointer;
+`
+export const CartButton = styled.button`
+    width: 100%;
+    padding: 1rem 0;
+    background-color: ${props => props.theme.colors.primary.orange};
+    outline: none;
+    border: none;
+    margin-top: 0.7rem;
+    border-radius: 8px;
+    font-weight: 700;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.4rem;
+    cursor: pointer;
+
+    img {
+        max-width: 18px;
+    }
+    p {
+        color: ${props => props.theme.colors.neutral.white};
+    }
+
+
 `
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,7 +1,10 @@
-import { HeroSection, HeroContainer, Slider, HeroContent, Next, Prev } from './Hero.styles';
+import { HeroSection, HeroContainer, Slider, HeroContent, Next, Prev, ContentHeader, Counter, Pricing, Price, Span1, Span2, Count, Minus, Plus, CartButton } from './Hero.styles';
 import img1 from '../../images/image-product-1.jpg';
 import prev from '../../images/icon-previous.svg';
 import next from '../../images/icon-next.svg';
+import minus from '../../images/icon-minus.svg';
+import plus from '../../images/icon-plus.svg';
+import cart from '../../images/icon-cart.svg';
 
 const Hero = () => {
   return ( 
@@ -18,9 +21,25 @@ const Hero = () => {
         </Slider>
 
         <HeroContent>
+          <ContentHeader>SNEAKER COMPANY</ContentHeader>
+          <h1>Fall Limited Edition Sneakers</h1>
+          <p>These low-profile sneakers are your perfect causal wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
 
+          <Counter>
+            <Pricing>
+                <Price><Span1>$125.00</Span1> <Span2>50%</Span2></Price>
+                <p>$250.00</p>
+            </Pricing>
+
+            <Count>
+              <Minus src={minus} alt='minus' />
+              <span>0</span>
+              <Plus src={plus} alt='plus'/>
+            </Count>
+
+            <CartButton><img src={cart} alt="cart" />  <p>Add to cart</p></CartButton>
+          </Counter>
         </HeroContent>
-
       </HeroContainer>
     </HeroSection>
   );
